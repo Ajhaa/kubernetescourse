9de30922cf47dd3123b509def40e588022fb8fdd
atte <atte.h.1998@gmail.com>
2020-07-15 13:52:44 +0300

    Save todos to stateful psql

--- todo_app/server/src/todoRouter.js
+++ todo_app/server/src/todoRouter.js
@@ -1,21 +1,38 @@
 const todoRouter = require('express').Router()
+const { Pool } = require('pg')
 
-const todos = [
-  { content: 'Just do it!', done: false },
-  { content: 'Time to stop!', done: false }
-]
+let pool = null
 
-todoRouter.get('/', (_, res) => {
-  res.json(todos)
+async function connect() {
+  try {
+    pool = new Pool()
+    await pool.query(
+      'CREATE TABLE IF NOT EXISTS todos (id serial PRIMARY KEY, content text NOT NULL, done boolean NOT NULL)'
+    )
+  } catch(e) {
+    console.log('Connection error, retrying\n', e)
+    setTimeout(connect, 5000)
+  }
+  
+}
+
+connect()
+
+todoRouter.get('/', async (_, res) => {
+  const dbRes = await pool.query('SELECT * FROM todos')
+
+  res.json(dbRes.rows)
 })
 
-todoRouter.post('/', (req, res) => {
+todoRouter.post('/', async (req, res) => {
   if (!req.body.todo) {
     return res.status(400).end()
   }
-  todos.push({ content: req.body.todo, done: false })
 
-  res.json(todos)
+  await pool.query('INSERT INTO todos (content, done) VALUES ($1, $2)', [req.body.todo, false])
+  const dbRes = await pool.query('SELECT * FROM todos')
+
+  res.json(dbRes.rows)
 })
 
 module.exports = todoRouter
